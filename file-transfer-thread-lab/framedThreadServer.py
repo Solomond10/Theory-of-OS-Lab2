db63dff1c856cc91e5011c89b22d7add270899ca
student <solomond10@yahoo.com>
2020-10-14 22:06:28 -0600

    Thread file transfer lab moved to file-transfer-lab. Framed Echo is restored so it has its original contents

--- file-transfer-thread-lab/framedThreadServer.py
+++ file-transfer-thread-lab/framedThreadServer.py
@@ -34,16 +34,32 @@ class Server(Thread):
         self.fsock = EncapFramedSock(sockAddr)
     def run(self):
         print("new thread handling connection from", self.addr)
-        while True:
+        fileInfo = (self.fsock.receive(debug)).decode()
+        print("The file's name, size and remote name file was received")
+        print("File Info: ", fileInfo)
+        fileName, fileSize, remoteFileName = fileInfo.split(":")
+        fileSize = int(fileSize)
+
+        #checks to see if file already exist
+        path = os.getcwd()+"/"+remoteFileName
+        if os.path.exists(path) is True:
+            print("The file is already on the server")
             payload = self.fsock.receive(debug)
-            if debug: print("rec'd: ", payload)
-            if not payload:     # done
-                if debug: print(f"thread connected to {addr} done")
-                self.fsock.close()
-                return          # exit
-            payload += b"!"             # make emphatic!
             self.fsock.send(payload, debug)
-        
+
+        else:
+            with open (remoteFileName, "w") as f:
+                print("File data is being recieved...")
+                payload = self.fsock.receive(debug)
+
+                #Decodes payload if it is not none and writes to the remote file
+                #If payload is nothing is none then it writes nothing to the remote file
+
+                if payload is not None:
+                    payloadDecoded = payload.decode()
+                    f.write(payloadDecoded)
+                    self.fsock.send(payload, debug)
+                    print("Exiting.....")    
 
 while True:
     sockAddr = lsock.accept()
